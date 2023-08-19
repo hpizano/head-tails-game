ce1fc7739194becef4aadd9eaf2c0c7d0c5d0209
Hector Pizano <pizano_hector@outlook.com>
2023-08-19 18:51:45 -0500

    Re wrote Yard Meters exercise

--- yard_meters.js
+++ yard_meters.js
@@ -1,1 +1,28 @@
-let inputYerds = windo.prompt()
+function convertToMeters(yards){
+    return yards * 0.9144;
+  }
+  
+  function createMessage (yards, meters) {
+    let message = '';
+    const numYards = yards * 1 ;
+    
+    if (numYards === 1760 ) {
+      message = 'That is as long as a mile.';
+    }
+    else if (numYards === 100) {
+      message = 'That is as long as a football field';
+    }
+    
+    else if (numYards === 26) {
+      message = 'That is as long as a Tennis Court';
+    }
+    return `${yards} yards is ${meters} meters. ${message}`;
+  }
+  
+  
+  let inputYards = window.prompt('Input an amount of Yards.');
+  let convertedMeters = convertToMeters(inputYards);
+  
+  let msg = createMessage(inputYards, convertedMeters);
+  
+  console.log(msg)
